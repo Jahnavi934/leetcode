2b0addfc932168017f2d61a828222b905ceda245
Jahnavi934 <96327245+Jahnavi934@users.noreply.github.com>
2024-01-30 21:29:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (30.19%) - LeetSync

---
+++ 1935-minimum-number-of-operations-to-reinitialize-a-permutation/minimum-number-of-operations-to-reinitialize-a-permutation.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reinitializePermutation(int n) {
+        vector<int> graph(n,0);
+        for (int i=0; i<n; ++i) 
+            graph[i]=i%2==0 ? i/2 : (i-1)/2+n/2;
+        int cur=graph[1], ans=1;
+        while (cur!=1) {cur=graph[cur]; ans++;}
+        return ans;
+    }
+};
