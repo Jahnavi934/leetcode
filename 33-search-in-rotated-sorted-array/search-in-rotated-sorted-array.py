f0452e5f1929c4c8fb33eb922c60a3a8a52e0fb6
Jahnavi934 <96327245+Jahnavi934@users.noreply.github.com>
2024-02-09 18:00:08 +0530

    Time: 17 ms (92.38%) | Memory: 11.9 MB (91.36%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in nums:
+            if i == target: return nums.index(i)
+        return -1
