9499cbdcde8b11edf48b3f25fdcdc37008306621
Jahnavi934 <96327245+Jahnavi934@users.noreply.github.com>
2024-03-04 18:36:14 +0530

    Time: 43 ms (54.86%) | Memory: 17.2 MB (35.88%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        list1 = []
+        list2 = []
+        for i in s:
+            list1.append(s.index(i))
+        for i in t:
+            list2.append(t.index(i))
+        if list1 == list2:
+            return True
+        else:
+            return False
